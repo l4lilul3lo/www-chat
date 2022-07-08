9999edb1be15cfd0d0ead00f8f5213401beef6fe
l4lilul3lo <logankirk989@gmail.com>
2022-07-07 21:21:35 -0700

    holy water

--- socketHandlers/roomHandler.js
+++ socketHandlers/roomHandler.js
@@ -2,6 +2,7 @@ const { createRoomDB } = require("../models/room");
 module.exports = (io, socket) => {
   async function createRoom(roomObj) {
     const room = await createRoomDB(roomObj);
+    // create rooms_users entry with new room data and the user who created it, with priveledge set to 2.
     io.emit("room:created", room);
   }
 
